b2b536f7fd8e7d320591e407d116c426a0366edf
Adit Yuwono Pratama <adit@agentpoint.com>
2024-09-12 15:07:02 +0700

    input fields

---
+++ runtest/uiComponents.spec.ts
@@ -0,0 +1,27 @@
+import {test, expect} from '@playwright/test'
+
+test.beforeEach(async({page}) => {
+    await page.goto('http://localhost:4200/')
+})
+
+test.describe('Form Layouts page', () => {
+    test.beforeEach(async({page}) => {
+        await page.getByText('Forms').click()
+        await page.getByText('Form layout').click()
+    })
+
+    test('input fields', async ({page}) => {
+        const usingTheGridEmailInput = page.locator('nb-card', {hasText: "Using the Grid"}).getByRole('textbox', {name: "Email"})
+
+        await usingTheGridEmailInput.fill('test@test.com')
+        await usingTheGridEmailInput.clear()
+        await usingTheGridEmailInput.pressSequentially('tes2@test.com', {delay: 1000})
+
+        //generic assertion
+        const inputValue = await usingTheGridEmailInput.inputValue()
+        expect(inputValue).toEqual('test2@test.com')
+
+        //locator assertion
+        await expect(usingTheGridEmailInput).toHaveValue('test2@test.com')
+    })
+})
