d6cc1a154a2624ab55b18b9b039f55e6a74eaa6c
Adit Yuwono Pratama <adit@agentpoint.com>
2024-09-13 08:34:09 +0700

    checkboxes

--- runtest/uiComponents.spec.ts
+++ runtest/uiComponents.spec.ts
@@ -39,3 +39,18 @@ test.describe('Form Layouts page', () => {
         expect (await usingTheGridForm.getByRole('radio', {name: "Option 2"}).isChecked()).toBeTruthy()
     })
 })
+
+test('checkboxes', async ({page}) => {
+    await page.getByText('Modal & Overlays').click()
+    await page.getByText('Toastr').click()
+
+    await page.getByRole('checkbox', {name: "Hide on click"}).uncheck({force: true})
+    await page.getByRole('checkbox', {name: "Prevent arising of duplicate toast"}).check({force: true})
+
+    //check all box (check = expect toBeTruthy || uncheck = expect toBeFalsy)
+    const allBoxes = page.getByRole('checkbox')
+    for(const box of await allBoxes.all()){
+        await box.check({force: true})
+        expect(await box.isChecked()).toBeTruthy()
+    }
+})
