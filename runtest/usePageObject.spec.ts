db1425674da916c7c34faa9e0bc74d17b963575b
Adit Yuwono Pratama <adit@agentpoint.com>
2024-11-01 10:19:51 +0700

    navigation page object

--- runtest/usePageObject.spec.ts
+++ runtest/usePageObject.spec.ts
@@ -9,4 +9,8 @@ test.beforeEach(async({page}) => {
 test('navigation to form page', async({page}) => {
     const navigateTo = new NavigationPage(page)
     await navigateTo.formLayoutsPage()
+    await navigateTo.datepickerPage()
+    await navigateTo.smartTablePage()
+    await navigateTo.toastrPage()
+    await navigateTo.tooltipPage()
 })
