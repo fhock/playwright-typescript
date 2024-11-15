59045436986129cde57ea6b3c2f46ef13f0959a7
Adit Yuwono Pratama <adit@agentpoint.com>
2024-11-15 11:21:12 +0700

    mocking API fix

--- runtest/workingWithAPI.spec.ts
+++ runtest/workingWithAPI.spec.ts
@@ -1,7 +1,15 @@
 import {test, expect} from '@playwright/test'
+import tags from '../test-data/tags.json'
 
-test.beforeEach(async ({page}) => {
+test.beforeEach( async({page}) => {
+    await page.route('*/**/api/tags', async route => {
+        await route.fulfill({
+            body: JSON.stringify(tags)
+        })
+    })
+    
     await page.goto('https://conduit.bondaracademy.com/')
+    await page.waitForTimeout(1000);
 })
 
 test('has title', async ({page}) => {
