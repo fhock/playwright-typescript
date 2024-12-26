aca391678e36a2e30934a6a9a617875248b52f95
Adit Yuwono Pratama <adit@agentpoint.com>
2024-12-26 09:15:06 +0700

    Modify API response

--- runtest/workingWithAPI.spec.ts
+++ runtest/workingWithAPI.spec.ts
@@ -7,6 +7,17 @@ test.beforeEach( async({page}) => {
             body: JSON.stringify(tags)
         })
     })
+
+    await page.route('*/**/api/articles*', async route => {
+        const response = await route.fetch()  
+        const responseBody = await response.json()
+        responseBody.articles[0].title = "This is a test title"  
+        responseBody.articles[0].description = "This is a description"
+
+        await route.fulfill({
+            body: JSON.stringify(responseBody)
+        })
+    })
     
     await page.goto('https://conduit.bondaracademy.com/')
     await page.waitForTimeout(1000);
@@ -14,4 +25,8 @@ test.beforeEach( async({page}) => {
 
 test('has title', async ({page}) => {
     await expect(page.locator('.navbar-brand')).toHaveText('conduit')
+    await expect(page.locator('app-article-list h1').first()).toContainText('This is a test title')
+    await expect(page.locator('app-article-list p').first()).toContainText('This is a description')
+    await page.waitForTimeout(1000);
+    
 })
